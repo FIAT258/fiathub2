7e2f69bd63f568c8cfbab20b87f085b78264a92b
Fiat <matheuscurcio23@gmail.com>
2026-01-21 18:33:00 -0300

    Update fiathubmuderlurph.lua

--- fiathubmuderlurph.lua
+++ fiathubmuderlurph.lua
@@ -1,158 +1,38 @@
--- RAYFIELD (BASE FUNCIONAL)
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-	Name = "FIAT HUB",
-	LoadingTitle = "FIAT HUB",
-	LoadingSubtitle = "by fiat",
-	ConfigurationSaving = {
-		Enabled = false
-	},
+	Name = "TESTE RAYFIELD",
+	LoadingTitle = "Teste",
+	LoadingSubtitle = "UI",
+	ConfigurationSaving = {Enabled = false},
 	KeySystem = false
 })
 
-------------------------------------------------
--- ABA FARM PRINCIPAL
-------------------------------------------------
-local FarmTab = Window:CreateTab("Farm principal", 4483362458)
+local Tab1 = Window:CreateTab("Farm principal", 4483362458)
 
-local FarmSection = FarmTab:CreateSection("Farms")
-
-FarmSection:CreateToggle({
-	Name = "Auto farm coins",
+Tab1:CreateToggle({
+	Name = "Toggle Teste",
 	CurrentValue = false,
 	Callback = function(v)
-		print("Auto farm coins:", v)
+		print("Toggle:", v)
 	end
 })
 
-FarmSection:CreateToggle({
-	Name = "Auto farm murderer",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Auto farm murderer:", v)
-	end
-})
-
-FarmSection:CreateParagraph({
-	Title = "Aviso",
-	Content = "Auto farms em beta, ative apenas um"
-})
-
-------------------------------------------------
--- ABA TROLL
-------------------------------------------------
-local TrollTab = Window:CreateTab("Troll", 4483362458)
-local TrollSection = TrollTab:CreateSection("Trollagens")
-
-TrollSection:CreateDropdown({
-	Name = "Selecionar player",
-	Options = {"Player1","Player2"},
-	CurrentOption = nil,
-	Callback = function(v)
-		print("Selecionado:", v)
-	end
-})
-
-TrollSection:CreateToggle({
-	Name = "Fling player específico",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Fling específico:", v)
-	end
-})
-
-TrollSection:CreateToggle({
-	Name = "Fling all",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Fling all:", v)
-	end
-})
-
-TrollSection:CreateButton({
-	Name = "Teleport Gun",
+Tab1:CreateButton({
+	Name = "Botão Teste",
 	Callback = function()
-		print("Teleport Gun")
+		print("Botão clicado")
 	end
 })
 
-TrollSection:CreateToggle({
-	Name = "Aim bot Murderer",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Aim bot:", v)
-	end
-})
+local Tab2 = Window:CreateTab("Discord", 4483362458)
 
-------------------------------------------------
--- ABA CONFIGURAÇÃO
-------------------------------------------------
-local ConfigTab = Window:CreateTab("Configuração", 4483362458)
-local ConfigSection = ConfigTab:CreateSection("Player")
-
-ConfigSection:CreateToggle({
-	Name = "ESP Sheriff/Killer",
-	CurrentValue = false,
-	Callback = function(v)
-		print("ESP Sheriff/Killer:", v)
-	end
-})
-
-ConfigSection:CreateToggle({
-	Name = "ESP Assassino",
-	CurrentValue = false,
-	Callback = function(v)
-		print("ESP Assassino:", v)
-	end
-})
-
-ConfigSection:CreateToggle({
-	Name = "Infinite Jump",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Infinite Jump:", v)
-	end
-})
-
-ConfigSection:CreateSlider({
-	Name = "Velocidade",
-	Range = {16,100},
-	Increment = 1,
-	CurrentValue = 16,
-	Callback = function(v)
-		print("Speed:", v)
-	end
-})
-
-ConfigSection:CreateToggle({
-	Name = "Ativar velocidade",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Speed toggle:", v)
-	end
-})
-
-ConfigSection:CreateToggle({
-	Name = "Noclip",
-	CurrentValue = false,
-	Callback = function(v)
-		print("Noclip:", v)
-	end
-})
-
-------------------------------------------------
--- ABA DISCORD
-------------------------------------------------
-local DiscordTab = Window:CreateTab("Discord", 4483362458)
-local DiscordSection = DiscordTab:CreateSection("Info")
-
-DiscordSection:CreateParagraph({
-	Title = "Informação",
+Tab2:CreateParagraph({
+	Title = "Info",
 	Content = "Script feito por fiat"
 })
 
-DiscordSection:CreateButton({
+Tab2:CreateButton({
 	Name = "Get Discord",
 	Callback = function()
 		setclipboard("https://discord.gg/VGRmqRRz")
