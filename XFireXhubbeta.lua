89fe2dc9a8ce4d829aa85cd7ba61b01948f3b6a0
Fiat <matheuscurcio23@gmail.com>
2026-02-07 18:51:54 -0300

    Update XFireXhubbeta.lua

--- XFireXhubbeta.lua
+++ XFireXhubbeta.lua
@@ -1,1 +1,7 @@
-print("hello")
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/brookhavenxfirexhub.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/minimizarfirex.lua"))()
+end)
