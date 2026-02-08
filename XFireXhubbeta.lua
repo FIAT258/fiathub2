c5c3045c6b4252bdb7614987c16e28eb835cb8d4
Fiat <matheuscurcio23@gmail.com>
2026-02-08 12:13:47 -0300

    Refactor script loading and remove unused variables

--- XFireXhubbeta.lua
+++ XFireXhubbeta.lua
@@ -1,27 +1,7 @@
-local totalScripts = 2
-local carregados = 0
-
-local inicio = tick()
-
-local function scriptCarregado()
-	carregados += 1
-	
-	if carregados >= totalScripts then
-		local tempo = tick() - inicio
-		print(string.format("%.2f segundos para scripts carregarem", tempo))
-	end
-end
-
 task.spawn(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/brookhavenxfirexhub.lua"))()
-	scriptCarregado()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/brookhavenxfirexhub.lua"))()
 end)
 
 task.spawn(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/minimizarfirex.lua"))()
-	scriptCarregado()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/minimizarfirex.lua"))()
 end)
-return {
-    Version = "1.11"
-}
-end
