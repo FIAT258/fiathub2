b44c6a8d049c6c472713197348a3685e2e8a11a3
Fiat <matheuscurcio23@gmail.com>
2026-02-07 21:35:17 -0300

    Load scripts and track loading time

--- XFireXhubbeta.lua
+++ XFireXhubbeta.lua
@@ -1,7 +1,27 @@
+local totalScripts = 2
+local carregados = 0
+
+local inicio = tick()
+
+local function scriptCarregado()
+	carregados += 1
+	
+	if carregados >= totalScripts then
+		local tempo = tick() - inicio
+		print(string.format("%.2f segundos para scripts carregarem", tempo))
+	end
+end
+
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/brookhavenxfirexhub.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/brookhavenxfirexhub.lua"))()
+	scriptCarregado()
 end)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/minimizarfirex.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/FIAT258/fiathub2/main/minimizarfirex.lua"))()
+	scriptCarregado()
 end)
+return {
+    Version = "1.11"
+}
+end
